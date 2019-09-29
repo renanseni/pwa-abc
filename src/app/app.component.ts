e48d3dd0808538a9224b2e9feb1984eebbd1bc8a
Renan Seni Souza <rsenisou@everis.com>
2019-09-29 14:16:25 -0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { google } from '@agm/core/services/google-maps-types';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'pwaBrowser';
+  title = 'PWA - Test';
+  latitude: any;
+  longitude: any;
+  @ViewChild('cameraView') videoElement: any;
+  video: any;
+
+  ngOnInit() {
+    this.video = this.videoElement.nativeElement;
+  }
+
+  openGeolocation() {
+    navigator.geolocation.getCurrentPosition((position) => {
+      this.latitude = +position.coords.latitude;
+      this.longitude = +position.coords.longitude;
+    });
+  }
+  openCamera() {
+    this.initCamera({ video: true, audio: false });
+  }
+
+  initCamera(config: any) {
+    var browser = <any>navigator;
+
+    browser.getUserMedia = (browser.getUserMedia ||
+      browser.webkitGetUserMedia ||
+      browser.mozGetUserMedia ||
+      browser.msGetUserMedia);
+
+    browser.mediaDevices.getUserMedia(config).then(stream => {
+      this.video.src = window.URL.createObjectURL(stream);
+      this.video.play();
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+import { AgmCoreModule } from '@agm/core';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +14,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyDvODtoHMyRSE1p6BV-BFvtBs2whJ4qLNs'      
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
